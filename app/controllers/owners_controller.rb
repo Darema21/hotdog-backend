b89bc2dfe9d09d21e4622d6c0cc20ea0cf1f6bf9
mbali thusi <mbali2c@gmail.com>
2023-07-08 15:33:35 +0800

    owner controller

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,28 @@
-class OwnersController < ApplicationController
+class  Api::V1::UsersController < Api::V1::BaseController
+  def show
+  end
+
+  def update
+    if @owner.update(owner_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def deactivate
+    @owner = Owner.find(params[:id])
+    @owner.update(status: false)
+  end
+
+  private
+
+  def owner_params
+    params.require(:owner).permit(:name, :age, :gender, :active)
+  end
+
+  def render_error
+    render json: { errors: @owner.errors.full_messages },
+    status: :unprocessable_entity
+  end
 end
