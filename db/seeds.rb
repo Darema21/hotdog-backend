75f9534a7220f446e0a8a49aaab9b1f41f72ae77
Darema Dzhold <darema.dzhold@outlook.com>
2023-07-15 12:18:09 +0800

    encry the ninjas key

--- db/seeds.rb
+++ db/seeds.rb
@@ -74,7 +74,7 @@ breed.save!
 
 # Creating breeds
 # base_url = "https://api.api-ninjas.com/v1/dogs?name="
-# api_key = { 'X-Api-Key' => 'Cqi1z+e5/4SUPNJX4yz3gA==RnQVVVLkceE3dqQC' }
+api_key = Rails.application.credentials.dig(:ninjas, :key)
 
 # breed_data.each do |breed_info|
 #   breed_name = breed_info[:name]
@@ -131,3 +131,42 @@ breed.save!
 #   end
 
 # end
+
+# db/seeds.rb
+
+# Disable the transactional behavior to improve performance
+ActiveRecord::Base.transaction do
+  # Create owners
+  owners = []
+
+  5.times do
+    owner = Owner.create(
+      name: Faker::Name.name,
+      age: Faker::Number.between(from: 18, to: 65),
+      gender: Faker::Gender.binary_type,
+      bio: Faker::Lorem.paragraph,
+      wechat_id: Faker::Alphanumeric.alphanumeric(number: 6),
+      active: true
+    )
+
+    owners << owner
+  end
+
+  # Create dogs and assign random owners
+  10.times do
+    owner = owners.sample
+
+    Dog.create(
+      name: Faker::Creature::Dog.name,
+      gender: Faker::Creature::Dog.gender,
+      age: Faker::Number.between(from: 1, to: 10),
+      neutered: Faker::Boolean.boolean,
+      vaccinated: Faker::Boolean.boolean,
+      address: Faker::Address.full_address,
+      bio: Faker::Lorem.paragraph,
+      owner: owner
+    )
+  end
+
+  puts "Seed data created successfully!"
+end
