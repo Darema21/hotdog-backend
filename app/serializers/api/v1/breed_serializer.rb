0ac4de0ff199511f6885acbaffa2da3ff942bd27
nastyam1994 <nastyam_94@bk.ru>
2023-07-15 22:54:46 +0800

    serializer

--- app/serializers/api/v1/breed_serializer.rb
+++ app/serializers/api/v1/breed_serializer.rb
@@ -49,26 +49,26 @@ class Api::V1::BreedSerializer < ActiveModel::Serializer
   end
 
   def calculate_similarity_score(other_breed)
-    # puts "----------asdads-----------------"
-    # puts object
-    # puts other_breed
-    # puts serial(other_breed)
-    # puts serial(other_breed).as_json
-    # puts serial(other_breed).friendliness_index
-    # puts "----------asdads-----------------"
+    puts "----------asdads-----------------"
+    puts object
+    puts other_breed
+    puts serial(other_breed)
+    puts serial(other_breed).as_json
+    puts serial(other_breed).friendliness_index
+    puts "----------asdads-----------------"
 
     friendliness_diff = ((object.good_with_children + object.good_with_other_dogs + object.good_with_strangers) / 15.0 - (other_breed["good_with_children"] + other_breed["good_with_other_dogs"] + other_breed["good_with_strangers"]) / 15.0).abs / 1.0
-    maintenance_diff = ((object.shedding + object.grooming + object.drooling) / 15.0 - (other_breed["shedding"] + other_breed["grooming"] + other_breed["drooling"]) / 15.0).abs / 1.0
-    activity_diff = ((object.playfulness + object.energy + object.barking) / 15.0 - (other_breed["playfulness"] + other_breed["energy"] + other_breed["barking"]) / 15.0).abs / 1.0
-    trainability_diff = (object.trainability / 5.0 - other_breed["trainability"] / 5.0).abs / 1.0
-    life_expectancy_diff = ((object.max_life_expectancy + object.min_life_expectancy) / 2.0 - (other_breed["max_life_expectancy"] + other_breed["min_life_expectancy"]) / 2.0).abs / 1.0
-    weight_diff = ((object.max_weight_male + object.min_weight_male + object.max_weight_female + object.min_weight_female) / 4.0 - (other_breed["max_weight_male"] + other_breed["min_weight_male"] + other_breed["max_weight_female"] + other_breed["min_weight_female"]) / 4.0).abs / 1.0
-    height_diff = (object.average_height - other_breed["average_height"]).abs / 1.0
+    # maintenance_diff = ((object.shedding + object.grooming + object.drooling) / 15.0 - (other_breed["shedding"] + other_breed["grooming"] + other_breed["drooling"]) / 15.0).abs / 1.0
+    # activity_diff = ((object.playfulness + object.energy + object.barking) / 15.0 - (other_breed["playfulness"] + other_breed["energy"] + other_breed["barking"]) / 15.0).abs / 1.0
+    # trainability_diff = (object.trainability / 5.0 - other_breed["trainability"] / 5.0).abs / 1.0
+    # life_expectancy_diff = ((object.max_life_expectancy + object.min_life_expectancy) / 2.0 - (other_breed["max_life_expectancy"] + other_breed["min_life_expectancy"]) / 2.0).abs / 1.0
+    # weight_diff = ((object.max_weight_male + object.min_weight_male + object.max_weight_female + object.min_weight_female) / 4.0 - (other_breed["max_weight_male"] + other_breed["min_weight_male"] + other_breed["max_weight_female"] + other_breed["min_weight_female"]) / 4.0).abs / 1.0
+    # height_diff = (object.average_height - other_breed["average_height"]).abs / 1.0
 
 
-    similarity_score = 1.0 - (friendliness_diff + maintenance_diff + activity_diff +
-                             trainability_diff + life_expectancy_diff +
-                             weight_diff + height_diff) / 7.0
+    # similarity_score = 1.0 - (friendliness_diff + maintenance_diff + activity_diff +
+    #                          trainability_diff + life_expectancy_diff +
+    #                          weight_diff + height_diff) / 7.0
     puts "friendliness_diff"
     friendliness_diff
   end
