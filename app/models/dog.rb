9cbbd494d455dfd3568652f06525dc53b007cbcc
Darema Dzhold <darema.dzhold@outlook.com>
2023-07-15 11:27:53 +0800

    saving current change

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,13 +2,13 @@ class Dog < ApplicationRecord
   belongs_to :owner
   has_many_attached :images
 
-  validates: name, presence: true, length: { minimum: 2}
-  validates: gender, presence: true
-  validates: age, presence: true
-  validates: neutered, presence: true
-  validates: vaccinated, presence: true
-  validates: bio, presence: true, , length: { minimum: 5}
-  validates: address
+  validates :name, presence: true, length: { minimum: 2}
+  validates :gender, presence: true
+  validates :age, presence: true
+  validates :neutered, presence: true
+  validates :vaccinated, presence: true
+  validates :bio, presence: true, length: { minimum: 5}
+  validates :address, presence: true
 end
 
 #we also need to add image later
