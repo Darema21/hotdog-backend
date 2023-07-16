51b78a4707c77bca6a56b1176ab9c4a776bd4102
nastyam1994 <nastyam_94@bk.ru>
2023-07-16 12:15:25 +0800

    Trying to add cloudinary

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,16 +1,14 @@
 class Breed < ApplicationRecord
   has_one_attached :image
 
-  def image_url
-    Rails.application.routes.url_helpers.rails_blob_url(image) if image.attached?
-  end
+  # def image_url
+  #   Rails.application.routes.url_helpers.rails_blob_url(image) if image.attached?
+  # end
 
-  # def photo_url
-  #   if photo.attached?
-  #     Rails.application.routes.url_helpers.rails_blob_url(photo)
-  #   else
-  #     # Default placeholder image URL or any other logic you prefer
-  #     'https://example.com/placeholder.jpg'
+  # def image_url
+  #   if image.attached?
+  #     Cloudinary::Utils.cloudinary_url(image.key) # Use the Cloudinary URL helper
   #   end
   # end
+
 end
